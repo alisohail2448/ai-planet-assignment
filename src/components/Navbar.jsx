f793846b409fd4fd9b4111623cab8a8db4ef5fbc
Sohail Akhtar Ali <alisohail2448@gmail.com>
2024-09-02 11:39:16 +0530

    Sohail | feat(setup): setup of material ui with react

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Container from "@mui/material/Container";
+import logo from "../assets/logo.png";
+
+function Navbar() {
+  return (
+    <AppBar position="static" color="#fff" sx={{ boxShadow: "none" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <img src={logo} alt="logo" width={100} />
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default Navbar;
